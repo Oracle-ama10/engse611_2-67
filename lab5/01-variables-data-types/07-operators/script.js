aae261d1769c821f46e68ee2ede827409528183c
phurinut <phurinut_te67@live.rmutl.ac.th>
2025-01-28 12:08:50 +0700

    bruh

---
+++ lab5/01-variables-data-types/07-operators/script.js
@@ -0,0 +1,46 @@
+
+// + - 
+// * /
+// div mod
+// ()
+//left to right
+
+
+//console.log( (1+2)*2-3 );
+
+//console.log( 6 == 0 );
+
+
+
+//console.log(2 == '2') // convert to sane type
+//console.log(2 === '2') // not convert to sane type
+
+//if (2 == '2') console.log("OK");
+
+//if ( !(2 === '2') ) console.log("Not tobe OK");
+
+
+//AND
+//0 0 = 0
+//0 1 = 0
+//1 0 = 0
+//1 1 = 1
+
+//OR
+//0  0 = 0
+//0  1 = 1
+//1  0 = 1
+//1  1 = 1
+
+//Not 
+//! 0 = 1
+//if (true && true ) console.log("OK");
+
+//if (false && false ) console.log("OR OK");
+
+//(3);
+
+if ( ( 1 == '1' ) && ( 3 == '3') && ( 4 === '4' ) ) console.log("AND OK");
+    console.log("OK");
+else
+    console.log("NOT OK");
